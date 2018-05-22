20802655b8c7b9c5d6861c55404389a5d59497b3
Ilya Ivantsov <iwww68@gmail.com>
2018-05-22 19:26:56 +0300

    New version of add_github

--- add_github.sh
+++ add_github.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-git add .
-git commit -m "$2"
-git $3
+function print_help() {
+    echo "  dir         Первый аргумент: название папки на github."
+    echo "  comment     Комментарий к коммиту."
+    echo "  tag         Тэг для коммита."
+    echo "  -y          Делать коммит. "
+    echo
+}
+if [ $# = 0 ]; then
+    print_help
+	exit 
+fi
+
+if [ "$2" == "-y" ]; then
+  git add .
+  git commit -m "$3"
+  git tag "$4"
+fi
+
+
 git remote add origin https://github.com/ilyaivantsov/"$1.git"
 git push -u origin master
